7784ea04fa1b115adc3f0a579ea8b3be6e477e77
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2026-05-29 21:31:10 +0600

    Database Design Steps

---
+++ database-design.ts
@@ -0,0 +1,9 @@
+//***Database Design - Step by Step***
+
+//1. Determining Entities
+//2. Determining Attributes for Each Entity
+//3. Relationships between Entities
+//4. Normalization of Database
+//5. Creating ER Diagram
+//6. Converting ER Diagram to Relational Schema
+//7. Implementing the Database using SQL
