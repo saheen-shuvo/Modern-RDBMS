c2d7289f360f5af0c61a2e90990b10eb773ad37f
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2026-05-29 20:49:09 +0600

    What is Database?

---
+++ what-is-database.ts
@@ -0,0 +1,6 @@
+//***What is Database?***
+//A database is an organized collection of data that is stored and accessed electronically. It allows for efficient storage, retrieval, and management of data. Databases are used in various applications, such as websites, applications, and businesses, to store and manage information. They can be structured (like relational databases) or unstructured (like NoSQL databases) depending on the type of data they handle.
+
+// ***Data vs Information***
+//Data refers to raw facts and figures that have not been processed or organized. It can be in the form of numbers, text, images, or any other type of unprocessed input. For example, a list of names and ages is considered data.
+//Information, on the other hand, is data that has been processed, interpreted, and organized in a way that makes it meaningful and useful for decision-making. For example, calculating the average age of the people in the list would result in information.
